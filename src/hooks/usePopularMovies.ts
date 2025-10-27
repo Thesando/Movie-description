cd4d40502b39c071613a9faf40061f9427ce368d
Thesando <alonso_sandoval1998@hotmail.com>
2025-10-27 00:53:55 -0600

    correcion de error

--- src/hooks/usePopularMovies.ts
+++ src/hooks/usePopularMovies.ts
@@ -6,7 +6,6 @@ export const usePopularMovies = () => {
     return useQuery<Movie[], Error>({
         queryKey: ['popularMovies'],
         queryFn: getPopularMovies,
-        staleTime: 1000 * 60 * 5,
-        cacheTime: 1000 * 60 * 10
-    })
-}
+        staleTime: 1000 * 60 * 5, // 5 minutos antes de volver a marcar stale
+    });
+};
